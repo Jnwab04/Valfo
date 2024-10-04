c43b3c0adfa355bea510db53f09726777dba0f17
uh chris <116903385+Chris-Kau@users.noreply.github.com>
2024-10-04 13:53:50 -0700

    made agents.py file and singleton agents class

---
+++ agents.py
@@ -0,0 +1,15 @@
+import requests
+class Agents():
+    __init_already__ = False
+    def __init__(self):
+        if not Agents.__init_already__:
+            Agents.__init_already__ = True
+            __url = "https://valorant-api.com/v1/agents"
+            __response = requests.get(__url).json()["data"]
+            self.response = __response
+        else:
+            print("Class already made")
+    def printAgents(self):
+        for agent in self.response:
+            if agent["isPlayableCharacter"]:
+                print(agent["displayName"])
