23b1eca2a21c0500d568ae37e27bb3bcba67ab84
Chris-Kau <uhchris04@gmail.com>
2024-10-05 22:18:30 -0700

    added method getAgent

--- agents.py
+++ agents.py
@@ -1,4 +1,5 @@
 import requests
+import discord
 class Agents():
     __init_already__ = False
     def __init__(self):
@@ -11,3 +12,22 @@ class Agents():
         for agent in self.response:
             if agent["isPlayableCharacter"]:
                 print(agent["displayName"])
+    def getAgent(self, name):
+        #capitalize first name to access API easier
+        name = (name[0].upper() + name[1:].lower()).strip()
+        agentDescription = None
+        agentImage = None
+        for agent in self.response:
+            if agent["isPlayableCharacter"] and agent["displayName"] == name:
+                agentDescription = agent["description"]
+                agentImage = agent["fullPortrait"]
+                break
+        agentEmbed = discord.Embed(
+            title= name,
+            color = discord.Color.green(),
+            type = 'rich',
+            description = agentDescription
+            )
+        agentEmbed.set_image(url = agentImage)
+
+        return agentEmbed
