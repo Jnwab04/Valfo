dbb6f3e0423fb3c0f0673ac45a2ea63490113afc
Chris-Kau <uhchris04@gmail.com>
2024-10-05 22:02:08 -0700

    change command bye to agentinfo

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 intents = discord.Intents.default()
 intents.message_content = True
 client = commands.Bot(command_prefix="/", intents = discord.Intents.all())
-
+agents = Agents()
 @client.event
 async def on_ready():
     try:
         synced = await client.tree.sync(guild = discord.Object(id = 1291847955963449374)) 
-        agents = Agents()
+        
         print(synced)
         print(f'Logged on as {client.user}')
     except Exception as e:
@@ -24,10 +24,13 @@ async def on_ready():
 @client.tree.command(name="help", description="Provides a list of commands (synced to our guild only)", guild = discord.Object(id = 1291847955963449374))
 async def help(interaction):
     await interaction.response.send_message("HEYY")
-@client.tree.command(name="bye", description="Provides information on every agent (synced to our guild only)", guild = discord.Object(id = 1291847955963449374))
-async def bye(interaction):
-    embed = discord.Embed(title="Agents", description="LIST OF AGENTS", color=discord.Colour.purple())
+
+@client.tree.command(name="agentinfo", description="Provides information on every agent (synced to our guild only)", guild = discord.Object(id = 1291847955963449374))
+async def agentinfo(interaction, agent: str):
+    embed = agents.getAgent(name = agent)
+    #embed.add_field(name = "test3", value = "value2", inline = False)
     await interaction.response.send_message(embed = embed)
+
 @client.tree.command(name="shutdown", description="force shut down the bot (synced to our guild only)" , guild = discord.Object(id = 1291847955963449374))
 async def shutdown(interaction):
     exit()
